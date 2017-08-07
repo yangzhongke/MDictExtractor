9c66194cdf80f87a3e85a02a9e8ccb0642ebb79a
zhongke yang <cownew@hotmail.com>
2017-08-07 17:22:19 +0800

    'init

---
+++ MDict提取/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace MDict提取
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnExtract = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.labelMsg = new System.Windows.Forms.Label();
+            this.labelCount = new System.Windows.Forms.Label();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnExtract
+            // 
+            this.btnExtract.Location = new System.Drawing.Point(15, 12);
+            this.btnExtract.Name = "btnExtract";
+            this.btnExtract.Size = new System.Drawing.Size(132, 27);
+            this.btnExtract.TabIndex = 0;
+            this.btnExtract.Text = "提取";
+            this.btnExtract.UseVisualStyleBackColor = true;
+            this.btnExtract.Click += new System.EventHandler(this.btnExtract_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "导入条数";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 86);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "当前导入";
+            // 
+            // labelMsg
+            // 
+            this.labelMsg.AutoSize = true;
+            this.labelMsg.Location = new System.Drawing.Point(72, 86);
+            this.labelMsg.Name = "labelMsg";
+            this.labelMsg.Size = new System.Drawing.Size(0, 12);
+            this.labelMsg.TabIndex = 3;
+            // 
+            // labelCount
+            // 
+            this.labelCount.AutoSize = true;
+            this.labelCount.Location = new System.Drawing.Point(72, 55);
+            this.labelCount.Name = "labelCount";
+            this.labelCount.Size = new System.Drawing.Size(0, 12);
+            this.labelCount.TabIndex = 4;
+            // 
+            // btnStop
+            // 
+            this.btnStop.Enabled = false;
+            this.btnStop.Location = new System.Drawing.Point(178, 13);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.TabIndex = 5;
+            this.btnStop.Text = "停止";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(522, 265);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.labelCount);
+            this.Controls.Add(this.labelMsg);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnExtract);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnExtract;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label labelMsg;
+        private System.Windows.Forms.Label labelCount;
+        private System.Windows.Forms.Button btnStop;
+    }
+}
+
---
+++ MDict提取/Form1.cs
@@ -0,0 +1,64 @@
+﻿using PInvoke;
+using System;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace MDict提取
+{
+    public partial class Form1 : Form
+    {
+        Thread thread;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void OnExtract(object hwndObj)
+        {
+            IntPtr hwndMain = (IntPtr)hwndObj;
+            int counter = 0;
+            while (true)
+            {
+                counter++;
+                User32.SendMessage(hwndMain, User32.WindowMessage.WM_COMMAND,
+                new IntPtr(1046), IntPtr.Zero);//【下一条】菜单项的id
+                User32.SendMessage(hwndMain, User32.WindowMessage.WM_COMMAND,
+                new IntPtr(1234), IntPtr.Zero);//【拷贝当前内容到粘贴板】菜单项的id
+                string text=null;
+                this.BeginInvoke(new Action(()=> {
+                    text = Clipboard.GetText();//不能在子线程中访问Clipboard
+                    text = Regex.Replace(text, @"\s", "");
+                    File.AppendAllText("d:/data.txt", text + "\r\n");
+                    labelCount.Text = counter.ToString();
+                    labelMsg.Text = text;
+                }) );
+                
+                Thread.Sleep(200);
+            }
+        }
+
+        private void btnExtract_Click(object sender, EventArgs e)
+        {
+            IntPtr hwndMain = User32.FindWindow("MDictMainWnd", "MDict");
+            if (hwndMain.ToInt32()==0)
+            {
+                MessageBox.Show("找不到MDict窗口");
+                return;
+            }
+            File.Delete("d:/data.txt");
+            thread = new Thread(new ParameterizedThreadStart(OnExtract));
+            thread.Start(hwndMain);
+            btnExtract.Enabled = false;
+            btnStop.Enabled = true;            
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            thread.Abort();
+            btnExtract.Enabled = true;
+            btnStop.Enabled = false;
+        }
+    }
+}
